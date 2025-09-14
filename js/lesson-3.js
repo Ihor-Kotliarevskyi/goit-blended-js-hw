b517949427a7f1dbd188e515b86d7e2ddad47a90
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-09-14 13:03:38 +0300

    additional tasks

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -9,6 +9,11 @@
 
 // console.log(newNumbers);
 
+// const newArrey = [];
+// numbers.forEach((element) => newArrey.push(element ** 2));
+
+// console.log(newArrey);
+
 // -------------------------------------------------------------------------------------------------------------------------------
 
 //2.
@@ -31,6 +36,10 @@
 
 // console.log(newArrey);
 
+// const getFlatArrey = (arr) => arr.flatMap((obj) => obj.values);
+
+// console.log(getFlatArrey(data));
+
 // -------------------------------------------------------------------------------------------------------------------------------
 
 //3.
@@ -44,10 +53,10 @@
 //   { name: "Bob", age: 19 },
 // ];
 
-// function check(years) {
-//   return people.some((ppl) => ppl.age < years);
+// function checkAge(arr, years) {
+//   return arr.some((obj) => obj.age < years);
 // }
-// console.log(check(20));
+// console.log(checkAge(people, 20));
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -57,20 +66,20 @@
 
 // const numbers = [2, 4, 6, 8, 10];
 
-// const checkNum = numbers.every((num) => !(num % 2));
+// const checkAllNumIsPair = (arr, value) => arr.every((item) => !(item % value));
 
-// console.log(checkNum);
+// console.log(checkAllNumIsPair(numbers));
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
 //5.
 // Знайдіть перше непарне число
 
-// const numbers = [2, 110, 6, 8, 94, 10, 121];
+// const numbers = [2, 110, 6, 8, 94, 10, 129];
 
-// const firstOddNum = numbers.find((num) => num % 2);
+// const firstOddNum = (arrey, value) => arrey.find((item) => item % value);
 
-// console.log(firstOddNum);
+// console.log(firstOddNum(numbers, 2));
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -80,9 +89,9 @@
 
 // const numbersArray = [4767, 287, 523, 15, 354, 2];
 
-// const sortNum = numbersArray.toSorted((a, b) => a - b);
+// const sortNum = (arrey) => arrey.toSorted((a, b) => a - b);
 
-// console.log(sortNum);
+// console.log(sortNum(numbersArray));
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -93,9 +102,9 @@
 
 // const stringArray = ["banana", "orange", "apple", "pear"];
 
-// const sortArrey = stringArray.sort((a, b) => a.localeCompare(b));
+// const sortArrey = (arrey) => arrey.sort((a, b) => a.localeCompare(b));
 
-// console.log(sortArrey);
+// console.log(sortArrey(stringArray));
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -109,9 +118,9 @@
 //   { name: "Bob", age: 19 },
 // ];
 
-// const sortUsers = users.toSorted((a, b) => a.age - b.age);
+// const sortUsers = (arrey) => arrey.toSorted((a, b) => a.age - b.age);
 
-// console.table(sortUsers);
+// console.table(sortUsers(users));
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -127,9 +136,9 @@
 //   { name: "Bob", age: 19 },
 // ];
 
-// const filteredUser = user.filter((item) => item.age > 20);
+// const filteredUser = (arrey) => arrey.filter((obj) => obj.age > 20);
 
-// console.table(filteredUser);
+// console.table(filteredUser(user));
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -139,9 +148,9 @@
 
 // const numbers = [1, 2, 3, 4, 5];
 
-// const sum = numbers.reduce((acc, num) => (acc += num), 0);
+// const sumNumbers = (array) => array.reduce((acc, num) => (acc += num), 0);
 
-// console.log(sum);
+// console.log(sumNumbers(numbers));
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -165,6 +174,47 @@
 // - метод multiply -Множить поточне значення на value. Повертає this.
 // Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
 // Приклад використання:
+
+// class Calculator {
+//   number(value) {
+//     this.result = value;
+//     return this;
+//   }
+//   add(value) {
+//     this.result += value;
+//     return this;
+//   }
+//   subtract(value) {
+//     this.result -= value;
+//     return this;
+//   }
+//   multiply(value) {
+//     this.result *= value;
+//     return this;
+//   }
+//   divide(value) {
+//     if (value !== 0) {
+//       this.result /= value;
+//       return this;
+//     }
+//     console.log("Value = 0, divide imposible!");
+//     return this;
+//   }
+//   getResult() {
+//     return this.result;
+//   }
+// }
+// const calc = new Calculator();
+
+// const result = calc
+//   .number(10)
+//   .add(5)
+//   .subtract(3)
+//   .multiply(4)
+//   .divide(2)
+//   .getResult();
+
+// console.log(result); // 24
 
 // class Calculator {
 //   constructor(value) {
@@ -207,19 +257,15 @@
 
 // const calc = new Calculator();
 
-// console.log(calc.startValue);
-// // const result = calc.number(5).subtract(1);
-// // console.log(calc);
-
 // const result = calc
-//   .number(100) // Встановлюємо початкове значення 10
-//   .add(85) // Додаємо 5 (10 + 5 = 15)
-//   .subtract(37) // Віднімаємо 3 (15 - 3 = 12)
-//   .multiply(44) // Множимо на 4 (12 * 4 = 48)
-//   .divide(10) // Ділимо на 2 (48 / 2 = 24)
-//   .getResult(); // Отримуємо результат: 24
+//   .number(100)
+//   .add(85)
+//   .subtract(37)
+//   .multiply(44)
+//   .divide(10)
+//   .getResult();
 
-// console.log(result); // 24
+// console.log(result);
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -232,10 +278,10 @@
 //   #login;
 //   #email;
 
-//   constructor(parameters) {
-//     this.#login = parameters.login;
-//     this.#email = parameters.email;
-//   }
+//   //   constructor(parameters) {
+//   //     this.#login = parameters.login;
+//   //     this.#email = parameters.email;
+//   //   }
 
 //   get login() {
 //     return this.#login;
@@ -254,12 +300,18 @@
 //   }
 // }
 
+// const client = new Client();
+// console.log(client.email);
+// console.log((client.email = "admin@mail.com"));
+// console.log(client.email);
+// console.log(client);
+
 // const mango = new Client({
 //   login: "Mango",
 //   email: "mango@mail.com",
 // });
 
-// console.log(mango); // { login: "Mango", email: "mango@mail.com" }
+// console.log(mango);
 // console.log(mango.login);
 // mango.login = "Super Mango";
 // console.log(mango.login);
@@ -267,7 +319,7 @@
 // console.log(mango.email);
 // mango.email = "super_mango@mail.com";
 // console.log(mango.email);
-// console.log(mango); // { login: "Super Mango", email: "super_mango@mail.com" }
+// console.log(mango);
 
 // -------------------------------------------------------------------------------------------------------------------------------
 
@@ -321,7 +373,6 @@
 //   email: "anna@example.com",
 // });
 // console.log(person1.getDetails());
-// // Очікувано: { name: "Anna", age: 25, gender: "female", email: "anna@example.com" }
 
 // // Перевірка класу Employee
 // const employee1 = new Employee({
@@ -333,9 +384,8 @@
 //   department: "IT",
 // });
 // console.log(employee1.getDetails());
-// // Очікувано: { name: "Ivan", age: 30, gender: "male", email: "ivan@example.com", salary: 2000, department: "IT" }
+
 // console.log(employee1.getEmployeeDetails());
-// // Очікувано: { salary: 2000, department: "IT" }
 
 // // Ще один співробітник
 // const employee2 = new Employee({
@@ -348,3 +398,94 @@
 // });
 // console.log(employee2.getDetails());
 // console.log(employee2.getEmployeeDetails());
+
+// -------------------------------------------------------------------------------------------------------------------------------
+
+// Додаткові завдання
+
+//Підрахунок кількості повторень
+//Дано масив:
+// //Завдання: За допомогою reduce підрахуйте, скільки разів кожен фрукт зустрічається в масиві.
+// //Очікуваний результат:
+// //{
+//  // apple: 3,
+//  // banana: 2,
+//  // orange: 1
+// //}
+
+// const fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+// const result = fruits.reduce((obj, string) => {
+//   obj[string] = (obj[string] || 0) + 1;
+//   return obj;
+// }, {});
+
+// console.table(result);
+
+// *********Task 1*********
+
+// Найбільше число в масиві
+//Дано масив чисел:
+//Завдання: Знайдіть найбільше число за допомогою reduce.
+//Очікуваний результат:
+//48
+
+// const numbers = [1009, 25, 48, 7, 32, 18];
+
+// const result = numbers.reduce((max, num) => {
+//   if (max < num) {
+//     return (max = num);
+//   }
+//   return max;
+// }, 0);
+// console.log(result);
+
+// *********Task 3*********
+// Напиши клас Notes, який керує колекцієї заміток у властивості items
+// Замітка - це об"єкт із властивостями text та priority
+// Додай класу статичну властивість PRIORITY,
+//     в якому буде зберігатися об"єкт з пріоритетами
+//     Додай методи addNote(note), removeNote(text), updatePriority(text, newPriority)
+
+class Notes {
+  static PRIORITY = {
+    LOW: low,
+    HI: hi,
+  };
+  constructor(parameters) {
+    this.text = parameters.text;
+    this.priority = parameters.priority || Notes.PRIORITY.LOW;
+  }
+  addNote(note) {
+    this.text;
+  }
+
+  removeNote(text) {}
+
+  updatePriority(text, newPriority) {}
+}
+
+// const note1 = new Notes()
+
+// note1.addNote({ text: 'Note1', priority: Notes.PRIORITY.LOW })
+// note1.addNote({ text: 'Note2', priority: Notes.PRIORITY.LOW })
+// note1.addNote({ text: 'Note3', priority: Notes.PRIORITY.LOW })
+// note1.removeNote('Note1')
+// note1.updatePriority('Note2', Notes.PRIORITY.HIGHT)
+// console.log(note1.items)
+//
+// *********Task 2*********
+// Напиши клас Notes, який керує колекцієї заміток у властивості items
+// Замітка - це об"єкт із властивостями text та priority
+// Додай класу статичну властивість PRIORITY,
+//     в якому буде зберігатися об"єкт з пріоритетами
+//     Додай методи addNote(note), removeNote(text), updatePriority(text, newPriority)
+
+// const note1 = new Notes()
+
+// note1.addNote({ text: 'Note1', priority: Notes.PRIORITY.LOW })
+// note1.addNote({ text: 'Note2', priority: Notes.PRIORITY.LOW })
+// note1.addNote({ text: 'Note3', priority: Notes.PRIORITY.LOW })
+// note1.removeNote('Note1')
+// note1.updatePriority('Note2', Notes.PRIORITY.HIGHT)
+// console.log(note1.items)
