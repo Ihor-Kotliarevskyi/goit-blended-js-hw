aa24e646a862ae9d77ce7f5d95737b4a5d9ac42e
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-09-26 22:22:32 +0300

    add markup and tasks

---
+++ js/lesson-4.js
@@ -0,0 +1,19 @@
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// // Деструктуризуємо
+// const { title, author, genres, isPublic, rating } = book;
+
+// // Використовуємо
+// const accessType = isPublic ? "pulbic" : "private";
+// const message = `Book ${title} by author ${author} with rating ${rating} is in ${accessType} access!`;
+
+// console.log(accessType);
+// console.log(message);
+// console.table(book);
+// console.table({ title, genres, isPublic, rating });
